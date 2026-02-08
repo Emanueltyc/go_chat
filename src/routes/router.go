55d5ec65ee8cfd047dcba5ff31fe421c89b4d697
Emanuel <jose.eosousa@gmail.com>
2026-02-08 19:16:16 -0300

    feat: removed slashes from routes

--- src/routes/router.go
+++ src/routes/router.go
@@ -8,17 +8,17 @@ import (
 )
 
 func RegisterRoutes(r *http.ServeMux, uc *controllers.UserController, cc *controllers.ChatController, mc *controllers.MessageController, hub *ws.Hub) {
-	r.HandleFunc("GET /user/", middlewares.Protect(uc.SearchUsers))
-	r.HandleFunc("POST /user/", uc.Register)
+	r.HandleFunc("GET /user", middlewares.Protect(uc.SearchUsers))
+	r.HandleFunc("POST /user", uc.Register)
 	r.HandleFunc("POST /user/login", uc.AuthUser)
 	r.HandleFunc("GET /user/info", middlewares.Protect(uc.Info))
 
-	r.HandleFunc("POST /chat/", middlewares.Protect(cc.AccessChat))
-	r.HandleFunc("GET /chat/", middlewares.Protect(cc.FetchChats))
+	r.HandleFunc("POST /chat", middlewares.Protect(cc.AccessChat))
+	r.HandleFunc("GET /chat", middlewares.Protect(cc.FetchChats))
 
-	r.HandleFunc("GET /messages/", middlewares.Protect(mc.Fetch))
+	r.HandleFunc("GET /messages", middlewares.Protect(mc.Fetch))
 
-	r.HandleFunc("GET /ws/", middlewares.Protect(func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("GET /ws", middlewares.Protect(func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWS(hub, w, r)
 	}))
 }
